ef7f67c8237652f30c6b95e022cff99ff48ffa17
carloserne <84287252+carloserne@users.noreply.github.com>
2024-05-18 18:11:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ async def read_form(request: Request):
 async def submit_form(username: str = Form(...), password: str = Form(...)):
     # Aquí puedes agregar lógica para procesar el usuario y contraseña
     return {"username": username, "password": password}
+
